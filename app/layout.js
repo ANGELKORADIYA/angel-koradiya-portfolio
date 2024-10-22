871dc8ada462663da7f52d0db3553309c203d545
Angel Koradiya <koradiyaangel11@gmail.com>
2024-10-22 19:55:03 +0530

    ads adss

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 import Navbar from "./navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,24 +14,24 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="google-adsense-account" content="ca-pub-8903937759165446" />
-        <Script
-          id="adsense-script"
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (adsbygoogle = window.adsbygoogle || []).push({});
+            `,
+          }}
+        />
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           data-ad-client="ca-pub-8903937759165446"
-          crossOrigin="anonymous"  // Fix: Ensure crossorigin matches the preload
+          crossOrigin="anonymous"
         />
-      
-        {/* <link rel="icon" href="https://perishablepress.com/wp/wp-content/images/2021/favicon-standard.png" />  */}
+        <link rel="icon" href="/path/to/your/favicon.ico" />
       </head>
       <body className={inter.className}>
         <Navbar />
         {children}
-        <Script
-          async
-          custom-element="amp-ad"
-          src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"
-        />
         <amp-ad
           width="100vw"
           height="320"
